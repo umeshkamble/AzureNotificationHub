85ed5873a0b83f50a40184de6eb711b13f8efda3
Umesh <ukamble6@gmail.com>
2024-11-10 16:32:40 +0530

    Update Config.cs

--- src/AzureNotificationMaui/Config.cs
+++ src/AzureNotificationMaui/Config.cs
@@ -9,6 +9,6 @@ namespace AzureNotificationMaui
     public static partial class Config
     {
         public static string ApiKey = "umeshkamble";
-        public static string BackendServiceEndpoint = "https://2kj4b385-44390.inc1.devtunnels.ms/";
+        public static string BackendServiceEndpoint = "https://2kj4b385-44390.inc1.devtunnels.ms/";//Used devtunnels url For more info please visit this url https://learn.microsoft.com/en-us/aspnet/core/test/dev-tunnels?view=aspnetcore-8.0
     }
 }
